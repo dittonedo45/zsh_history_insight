ed8e7330d90d5f229f729cfb1cf28b36fe5445a5
Eddington Chamunorwa <dittonedo45@gmail.com>
2024-11-29 10:13:14 +0200

    Capture the time and the unkown flag plus cmdline (Debugging)

--- main.cpp
+++ main.cpp
@@ -10,15 +10,22 @@ void parse_cmd(string cmd)
 int main ()
 {
 	string line;
+	string cmdline{};
+
 	while (getline(cin, line))
 	{
 		if (line[0]==':')
 		{
+			line=line.substr(1);
 			int sep1=line.find(':');
 			int sep2=line.find(';');
-			if (sep1==-1 || sep2==-1) continue;
-			cout<<line<<endl;
 
+			if (sep1==-1 || sep2==-1) continue;
+			string ltime{line.substr(1, sep1-1)};
+
+			string hflag{line.substr(sep1+1, sep2-sep1-1)};
+
+			cout<<ltime<<" "<<hflag<<endl;
 		}
 	}
 	return 0;
