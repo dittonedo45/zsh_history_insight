d3d8f536db6edeac8b295f8c36cd666a8cdf4876
Eddington Chamunorwa <dittonedo45@gmail.com>
2024-11-29 10:12:16 +0200

    strchr on strings is kinda weird

--- main.cpp
+++ main.cpp
@@ -12,7 +12,14 @@ int main ()
 	string line;
 	while (getline(cin, line))
 	{
-		cout<<line<<endl;
+		if (line[0]==':')
+		{
+			int sep1=line.find(':');
+			int sep2=line.find(';');
+			if (sep1==-1 || sep2==-1) continue;
+			cout<<line<<endl;
+
+		}
 	}
 	return 0;
 }
